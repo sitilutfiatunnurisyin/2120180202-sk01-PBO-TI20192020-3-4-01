9b992bf45a97e6bb80d25e23b62d22b498a51c86
sitilutfiatunnurisyin <55843840+sitilutfiatunnurisyin@users.noreply.github.com>
2019-09-26 21:30:45 +0700

    upload tugas 1

---
+++ src/pkg2120180202/sk01/pbo/ti20192020/pkg3/pkg4/pkg01/Sk01PBOTI201920203401.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2120180202.sk01.pbo.ti20192020.pkg3.pkg4.pkg01;
+
+/**
+ *
+ * @author asus
+ */
+public class Sk01PBOTI201920203401 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String b = "1";
+        for (int a = 1; a <= 9; a++) {
+            for(int c = 1; c <= a; c++){
+                System.out.print(b);
+            }
+            System.out.println();
+        }
+    }
+    
+}
